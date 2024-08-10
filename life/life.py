cbde081c7440ac4477ccdad1c88443b37e770146
Ben Hopkins <32267184+MaihgrUmosch@users.noreply.github.com>
2024-08-10 11:10:20 +0100

    Apply most flake8 changes.

--- life/life.py
+++ life/life.py
@@ -1,14 +1,20 @@
+"""This is the "life" module within the "Chapter 4 Package"."""
+
 import numpy as np
 from matplotlib import pyplot
 from scipy.signal import convolve2d
 
-glider = np.array([[0,1,0],[0,0,1],[1,1,1]])
+glider = np.array([
+    [0, 1, 0],
+    [0, 0, 1],
+    [1, 1, 1]
+])
 
 blinker = np.array([
-[0,0,0],
-[1,1,1],
-[0,0,0]]
-    )
+    [0, 0, 0],
+    [1, 1, 1],
+    [0, 0, 0]
+])
 
 glider_gun = np.array([
     [0, 0, 0, 0, 1, 1, 0, 0, 0],
@@ -49,30 +55,50 @@ glider_gun = np.array([
     [0, 0, 1, 1, 0, 0, 0, 0, 0]
 ])
 
+
 class Game:
-    def __init__(game,Size):
-        game.board = np.zeros((Size, Size))
-    
+    """A Game of Life."""
+
+    def __init__(self, size):
+        """Set up the Game of life with a grid size of size x size."""
+        self.board = np.zeros((size, size))
+
     def play(self):
+        """Set the Game of life into motion."""
         print("Playing life. Press ctrl + c to stop.")
         pyplot.ion()
         while True:
-            self.move(); self.show()
+            self.move()
+            self.show()
             pyplot.pause(0.0000005)
 
     def move(self):
-        STENCIL = np.array([[1, 1, 1], [1, 0, 1], [1, 1, 1]])
-        NeighbourCount = convolve2d(self.board, STENCIL, mode='same')
+        """Need to determine what this does..."""
+
+        stencil = np.array([[1, 1, 1], [1, 0, 1], [1, 1, 1]])
+        neighbour_count = convolve2d(self.board, stencil, mode='same')
 
         for i in range(self.board.shape[0]):
             for j in range(self.board.shape[1]):
-                self.board[i,j]=1 if (NeighbourCount[i, j]==3 or (NeighbourCount[i, j]==2 and self.board[i,j])) else 0
+                self.board[i, j] = (
+                    1 if (
+                    # Needs a why comment
+                        neighbour_count[i, j] == 3
+                        or (
+                            neighbour_count[i, j] == 2
+                            and
+                            self.board[i, j]
+                        )
+                    )
+                    else 0
+                )
 
-    def __setitem__(self, key, value): 
+    def __setitem__(self, key, value):
+        """Overwrite setter for item."""
         self.board[key] = value
 
-
     def show(self):
-        pyplot.clf() 
+        """Show the game on a pyplot."""
+        pyplot.clf()
         pyplot.matshow(self.board, fignum=0, cmap='binary')
         pyplot.show()
